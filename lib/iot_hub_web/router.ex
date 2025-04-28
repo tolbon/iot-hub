4097bb0fe3e2f2d9e8506e4eeff39bdf2ecc557d
Jean-Baptiste Noblot <jbnoblot@consignity.fr>
2025-04-28 14:56:37 +0200

    user + phx.release.gen

--- lib/iot_hub_web/router.ex
+++ lib/iot_hub_web/router.ex
@@ -1,6 +1,8 @@
 defmodule IotHubWeb.Router do
   use IotHubWeb, :router
 
+  import IotHubWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule IotHubWeb.Router do
     plug :put_root_layout, html: {IotHubWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -70,6 +73,44 @@ defmodule IotHubWeb.Router do
 
       live_dashboard "/dashboard", metrics: IotHubWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", IotHubWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{IotHubWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", IotHubWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{IotHubWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", IotHubWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{IotHubWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\iot_hub" eval IotHub.Release.migrate
